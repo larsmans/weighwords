e0e32a94d93952d9877b8ec17625885ff8797426
Lars Buitinck <L.J.Buitinck@uva.nl>
2011-05-27 17:10:13 +0200

    Nice and shiny LM object
    Needs a better interface: merge document_model and EM into one

--- parsimonious.py
+++ parsimonious.py
@@ -1,74 +1,128 @@
 #!/usr/bin/env python
 
+# Parsimonious language models
+#
+# As per D. Hiemstra, S. Robertson and H. Zaragoza (2004). Parsimonious
+# language models for information retrieval. SIGIR'04.
+#
+# Author: Lars Buitinck, ILPS, U. Amsterdam.
+
 from collections import defaultdict
 import logging
 import numpy as np
 
 
-def corpus_model(documents):
-    logging.info('Gathering corpus statistics')
-
-    vocab = {}              # Vocabulary: maps terms to numeric indices
-    cf = defaultdict(int)   # Corpus frequency
-
-    for d in documents:
-        for ln in d:
-            tok, pos, freq, rest = ln.split()
-            i = vocab.setdefault((tok, pos), len(vocab))
-            cf[i] += int(freq)
-
-    c_size = np.log2(sum(cf.itervalues()))
-
-    p_corpus = np.zeros(len(vocab))   # log P(t|C)
-    for i, f in cf.iteritems():
-        p_corpus[i] = np.log2(f) - c_size
-
-    return vocab, p_corpus
+logger = logging.getLogger(__name__)
 
 
-def document_model(d, vocab):
-    '''Returns (tf, p_term)
+class ParsimoniousLM:
+    def __init__(self, documents):
+        '''Build corpus (background) model.
 
-    Initial p_term is 1/n_distinct for terms with non-zero tf,
-    0 for terms with 0 tf.'''
+        Parameters
+        ----------
+        documents : array of arrays of terms
 
-    logging.info('Gathering term probabilities')
+        Returns
+        -------
+        vocab : dict of term -> int
+            Mapping of terms to numeric indices
+        p_corpus : array of float
+            Log prob of terms
+        '''
 
-    tf = np.zeros(len(vocab))   # Term frequency
-    p_term = np.empty(len(tf))
-    p_term.fill(-np.inf)        # lg 0
+        logger.info('Building corpus model')
 
-    n_distinct = 0
-    for ln in d:
-        tok, pos, freq, rest = ln.split()
-        i = vocab[(tok, pos)]
-        if tf[i] == 0:
-            p_term[i] = 0.      # lg 1
-            n_distinct += 1
-        tf[i] += float(freq)
+        self.vocab = {}              # Vocabulary: maps terms to numeric indices
+        cf = defaultdict(int)   # Corpus frequency
 
-    p_term -= np.log2(n_distinct)
+        for d in documents:
+            for tok in d:
+                i = vocab.setdefault(tok, len(vocab))
+                cf[i] += 1
 
-    return tf, p_term
+        c_size = np.log(sum(cf.itervalues()))
+
+        self.p_corpus = np.zeros(len(vocab))    # log P(t|C)
+        for i, f in cf.iteritems():
+            p_corpus[i] = np.log(f) - c_size
 
 
-def EM(tf, p_corpus, p_term, w_unigrams, w_bigrams, n_iter):
-    '''Expectation maximization'''
+    def document_model(self, d, vocab):
+        '''Build document model.
 
-    logging.info('EM')
+        Parameters
+        ----------
+        d : array of terms
+        vocab : see @corpus_model
 
-    w_ = np.log2(1 - w_unigrams - w_bigrams)
-    w_unigrams, w_bigrams = np.log2((w_unigrams, w_bigrams))
+        Returns
+        -------
+        tf : array of int
+            Term frequencies
+        p_term : array of float
+            Term log probabilities
 
-    p_corpus += w_
-    tf = np.log2(tf)
+        Initial p_term is 1/n_distinct for terms with non-zero tf,
+        0 for terms with 0 tf.
+        '''
 
-    for i in xrange(n_iter):
-        # E-step
-        p_term += w_unigrams
-        E = tf + p_term - np.logaddexp2(p_corpus, p_term)
+        logger.info('Gathering term probabilities')
 
-        # M-step
-        p_term = E - np.logaddexp2.reduce(E)
+        tf = np.zeros(len(vocab))   # Term frequency
+        p_term = np.empty(tf.shape[0])
+        p_term.fill(-np.inf)        # lg 0
 
-    return p_term
+        n_distinct = 0
+        for tok in d:
+            i = self.vocab[tok]
+            if tf[i] == 0:
+                p_term[i] = 0.      # lg 1
+                n_distinct += 1
+            tf[i] += 1.
+
+        p_term -= np.log(n_distinct)
+
+        return tf, p_term
+
+
+    def EM(self, tf, p_term, w, n_iter=50):
+        '''Expectation maximization.
+
+        Parameters
+        ----------
+        tf : array of float
+            Term frequencies, as returned by document_model
+        p_term : array of float
+            Term probabilities, as returned by document_model
+        w : float
+            Weight of document model (1 - weight of corpus model)
+        n_iter : int
+            Number of iterations to run.
+
+        Returns
+        -------
+        p_term : array of float
+            A posteriori term probabilities.
+        '''
+
+        logger.info('EM')
+
+        w_ = np.log(1 - w)
+        w = np.log(w)
+
+        p_corpus = self.p_corpus + w_
+        tf = np.log(tf)
+
+        E = np.empty(tf.shape[0])
+
+        p_term = np.array(p_term)
+        for i in xrange(n_iter):
+            # E-step
+            p_term += w
+            E = tf + p_term - np.logaddexp(p_corpus, p_term)
+
+            # M-step
+            p_term = E - np.logaddexp.reduce(E)
+
+        return p_term
