28e0b9d4a711f7ac9315743ebcd9bfb1e4386ff8
Lars Buitinck <L.J.Buitinck@uva.nl>
2011-05-31 17:16:06 +0200

    Add setuptools infrastructure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'weighwords',
+    version = '0.0',
+    author = 'Lars Buitinck',
+    author_email = 'L.J.Buitinck@uva.nl',
+    description = 'Python library for creating word weights/word clouds from text',
+    keywords = 'word cloud nlp',
+    packages = ['weighwords'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+    ]
+)
